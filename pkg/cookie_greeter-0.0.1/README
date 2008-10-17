238c40085e649d2a8105372e1aee2e58373a3b52
Nando <fernando.garcia@the-cocktail.com>
2008-10-17 22:16:02 +0200

    Initial release

---
+++ pkg/cookie_greeter-0.0.1/README
@@ -0,0 +1,36 @@
+Cookie Greeter: personal greeting based on a cookie
+
+Description:
+    This JavaScript project is a simple function that pretend to say hello to
+    the user based on the presence of a cookie. If the cookie does not exists
+    the anonymous elements are shown.
+
+    CookieGreeter.greet receives three arguments:
+     - the cookie name: the name of the cookie with the name of the user
+     - the anonymous element/s id/s: that will be shown if the cookie is not
+       set. You can send a single id or an array of ids.
+     - the personal element/s id/s: thet will be shown if the cookie has a
+       value. All ocurrences of -the cookie name- will be replaced with the
+       cookie value. You can use a single id or an array of ids.
+
+Example:
+    CookieGreeter.greet('greeter_username', 
+      ['anonymous_greeter', 'create_your_account_now']
+      'personal_greeter');
+
+    This will look for the cookie 'greeter_username' and then:
+    - if the cookie doesn't exist (or is blank) shows the 'anonymous_greeter'
+      and 'create_your_account_now' elements and hides 'personal_greeter'.
+    - if the cookie has a value hides 'anonymous_greeter' and
+      'create_your_account_now' elements and show the 'personal_greeter'
+      element after replacing the text '-greeter_username-' with that value.
+
+More information:
+    See test/unit/cookie_greeter_test.html for more examples.
+
+Thanks:
+    To Choan Galvez for making me happier using Jshoulda while writing this
+    code. 
+    
+Author:
+    Fernando García Samblas
---
+++ pkg/cookie_greeter-0.0.1/dist/cookie_greeter.js
@@ -0,0 +1,64 @@
+/*  Cookie Greeter, version 0.0.1
+ *  (c) 2008 Fernando García Samblas <fernando.garcia@the-cocktail.com>
+ *
+ *  Cookie Greeter is freely distributable under
+ *  the terms of an MIT-style license.
+ *
+ *  For details see the repo at http://github.com/nando/cookie-greeter
+ *
+ *--------------------------------------------------------------------------*/
+
+var CookieGreeterMeta = {
+  Version: '0.0.1'
+};
+
+
+var CookieGreeter = {
+  element: function(id) { return document.getElementById(id) },
+  readCookie: function(name) {
+    var all_cookies = document.cookie;
+    var pos = all_cookies.indexOf(name + '=');
+    if (pos != -1) {
+      var start_at = pos + name.length + 1;
+      var end_at = all_cookies.indexOf(";", start_at);
+      if (end_at == -1) end_at = all_cookies.length;
+      var value = all_cookies.substring(start_at, end_at);
+      return decodeURIComponent(value);
+    }
+    else
+      return false;
+  },
+  setDisplays: function(array_of_ids, display_value) {
+    var e;
+    if (typeof(array_of_ids) == "string") array_of_ids = [ array_of_ids ];
+    for (var i in array_of_ids) {
+      e = CookieGreeter.element(array_of_ids[i]);
+      if (e) e.style.display = display_value;
+    };
+  },
+  showElements: function(array_of_ids) {
+    CookieGreeter.setDisplays(array_of_ids, 'block');
+  },
+  hideElements: function(array_of_ids) {
+    CookieGreeter.setDisplays(array_of_ids, 'none');
+  },
+  greet: function(name, anonymous_ids, personal_ids) {
+    var value = CookieGreeter.readCookie(name);
+    if (value) {
+      CookieGreeter.hideElements(anonymous_ids);
+      if (typeof(personal_ids) == "string") personal_ids = [ personal_ids ];
+      for (var i in personal_ids) {
+        e = CookieGreeter.element(personal_ids[i]);
+        if (e) {
+          var re = new RegExp('-' + name + '-', "g");
+          e.innerHTML = e.innerHTML.replace(re, value);
+          e.style.display = 'block';
+        };
+      };
+    }
+    else {
+      CookieGreeter.hideElements(personal_ids);
+      CookieGreeter.showElements(anonymous_ids);
+    }
+  }
+};
